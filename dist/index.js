218a7bad631f7d3b4f45c964f70045638655213c
dosepehr <dosepehr@gmail.com>
2023-06-02 02:53:27 +0330

    const assertion

--- dist/index.js
+++ dist/index.js
@@ -1,5 +1,9 @@
 "use strict";
-let test = 'sepehr';
-// let username = <string>test;
-let username = test;
+const nums = [1, 2];
+let obj = { name: 'sepehr' };
+// nums.push(12) --> error
+const sum = (num1, num2) => {
+    return num1 + num2;
+};
+sum(...nums); // if we remove (as const) in line 1, we get error
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,11 @@
-let test: unknown = 'sepehr';
+const nums = [1, 2] as const;
 
-// let username = <string>test;
-let username = test as string;
+let obj: { readonly name: string } = <const>{ name: 'sepehr' };
+
+// nums.push(12) --> error
+
+const sum = (num1: number, num2: number): number => {
+    return num1 + num2;
+};
+
+sum(...nums); // if we remove (as const) in line 1, we get error
