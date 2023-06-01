ee707b42edeb3d5bf28293731c2f5a8e53f1fd2c
dosepehr <dosepehr@gmail.com>
2023-06-02 00:09:00 +0330

    ts types

--- dist/index.js
+++ dist/index.js
@@ -1,11 +1,24 @@
 "use strict";
 /*
-- make a property optional
-- make a property readonly
+- make a type
+- union types
 */
-const obj = {
+const user1 = {
     id: 1,
     name: 'sepehr',
     // age: 21,
+    deleteUser: (id) => {
+        return `user of ${id} deleted`;
+    },
 };
+const user2 = {
+    id: 2,
+    name: 'ali',
+    // age: 21,
+    deleteUser: (id) => {
+        return `user of ${id} deleted`;
+    },
+};
+// user1 must have at least one properties of User """OR""" Admin
+// user2 must have all properties of User """AND""" Admin
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,36 @@
 /*
-- make a property optional
-- make a property readonly
+- make a type
+- union types
 */
 
-const obj: {
+type User = {
     readonly id: number;
     name: string;
     age?: number;
-} = {
+};
+
+type Admin = {
+    deleteUser: (id: number) => string;
+};
+
+const user1: User | Admin = {
     id: 1,
     name: 'sepehr',
     // age: 21,
+    deleteUser: (id: number) => {
+        return `user of ${id} deleted`;
+    },
 };
+
+const user2: User & Admin = {
+    id: 2,
+    name: 'ali',
+    // age: 21,
+    deleteUser: (id: number) => {
+        return `user of ${id} deleted`;
+    },
+};
+
+
+// user1 must have at least one properties of User """OR""" Admin
+// user2 must have all properties of User """AND""" Admin
