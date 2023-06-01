58a52e9b81c8adc89ee994f503ae8f3b00d6873c
dosepehr <dosepehr@gmail.com>
2023-06-02 00:11:36 +0330

    ts literal types

--- dist/index.js
+++ dist/index.js
@@ -1,24 +1,5 @@
 "use strict";
-/*
-- make a type
-- union types
-*/
-const user1 = {
-    id: 1,
-    name: 'sepehr',
-    // age: 21,
-    deleteUser: (id) => {
-        return `user of ${id} deleted`;
-    },
-};
-const user2 = {
-    id: 2,
-    name: 'ali',
-    // age: 21,
-    deleteUser: (id) => {
-        return `user of ${id} deleted`;
-    },
-};
-// user1 must have at least one properties of User """OR""" Admin
-// user2 must have all properties of User """AND""" Admin
+// literal types
+let size = 'in';
+// size can be only 'cm' or 'in'
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -1,36 +1,7 @@
-/*
-- make a type
-- union types
-*/
+// literal types
 
-type User = {
-    readonly id: number;
-    name: string;
-    age?: number;
-};
+type Metric = 'cm' | 'in';
 
-type Admin = {
-    deleteUser: (id: number) => string;
-};
+let size: Metric = 'in';
 
-const user1: User | Admin = {
-    id: 1,
-    name: 'sepehr',
-    // age: 21,
-    deleteUser: (id: number) => {
-        return `user of ${id} deleted`;
-    },
-};
-
-const user2: User & Admin = {
-    id: 2,
-    name: 'ali',
-    // age: 21,
-    deleteUser: (id: number) => {
-        return `user of ${id} deleted`;
-    },
-};
-
-
-// user1 must have at least one properties of User """OR""" Admin
-// user2 must have all properties of User """AND""" Admin
+// size can be only 'cm' or 'in'
