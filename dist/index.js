67461df5edc1cde17e32855251af3784880b05c9
dosepehr <dosepehr@gmail.com>
2023-06-03 19:03:02 +0330

    ts interfaces

--- dist/index.js
+++ dist/index.js
@@ -1,35 +1,13 @@
 "use strict";
-// refactor constructor
-// make optional property
-class Person {
-    constructor(name, email, age) {
-        this.name = name;
-        this.email = email;
-        this.age = age;
-    }
-    get getEmail() {
-        return this.email;
-    }
-    set setEmail(email) {
-        this.email = email;
-    }
-    static score() {
-        console.log('score');
-    }
-}
-class Student extends Person {
-    constructor(avg, name, email, age) {
-        super(name, email, age);
-        this.avg = avg;
-        this.setAge = (Age) => {
-            this.age = Age;
-        };
-    }
-    get getEmail() {
-        return this.name;
-    }
-}
-const sepehr = new Person('sepehr', 'sepehr@gmail.com', 21);
-sepehr.address = 'karaj';
-const vahid = new Student(19.53, 'vahid', 'vahid@gmail.com', 30);
+let ali = {
+    name: 'ali',
+    // email: 'ali@gmail.com',
+    login() {
+        if (this.name == 'sepehr') {
+            return true;
+        }
+        return false;
+    },
+};
+console.log(ali.login());
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -1,38 +1,22 @@
-// refactor constructor
-// make optional property
-class Person {
-    address?: string;
-    constructor(
-        public name: string,
-        private email: string,
-        protected age: number
-    ) {}
-    get getEmail() {
-        return this.email;
-    }
-    set setEmail(email: string) {
-        this.email = email;
-    }
-    static score() {
-        console.log('score');
-    }
+// diffrence betweemn type and interface
+// 1 - interface can be modified
+// 2 - interface can be inherited
+interface User {
+    name: string;
+    email?: string; //making property optional
+
+    login(): boolean;
 }
 
-class Student extends Person {
-    constructor(public avg: number, name: string, email: string, age: number) {
-        super(name, email, age);
-    }
-    setAge = (Age: number): void => {
-        this.age = Age;
-    };
-    override get getEmail() {
-        return this.name;
-    }
-}
+let ali: User = {
+    name: 'ali',
+    // email: 'ali@gmail.com',
+    login() {
+        if (this.name == 'sepehr') {
+            return true;
+        }
+        return false;
+    },
+};
 
-const sepehr = new Person('sepehr', 'sepehr@gmail.com', 21);
-sepehr.address='karaj'
-const vahid = new Student(19.53, 'vahid', 'vahid@gmail.com', 30);
-
-
-
+console.log(ali.login());
